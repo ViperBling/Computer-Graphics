2793b094d79c1d97c325fedc20f35c20aa94bad0
Artorias <zero@ArtoriasMacBook-Pro.local>
2020-11-26 16:16:37 +0800

    update Bilinear

--- GAMES101/Homework/Assignment.3/Code/Texture.hpp
+++ GAMES101/Homework/Assignment.3/Code/Texture.hpp
@@ -24,10 +24,48 @@ public:
 
     Eigen::Vector3f getColor(float u, float v)
     {
+        u = u < 0 ? 0 : u;
+        u = u > 1 ? 1 : u;
+        v = v < 0 ? 0 : v;
+        v = v > 1 ? 1 : v;
         auto u_img = u * width;
         auto v_img = (1 - v) * height;
         auto color = image_data.at<cv::Vec3b>(v_img, u_img);
         return Eigen::Vector3f(color[0], color[1], color[2]);
+    }
+
+    Eigen::Vector3f getColorBilinear(float u, float v)
+    {
+        u = u < 0 ? 0 : u;
+        u = u > 1 ? 1 : u;
+        v = v < 0 ? 0 : v;
+        v = v > 1 ? 1 : v;
+        auto u_img = u * width;
+        auto v_img = (1 - v) * height;
+
+        // 相当于取一个在u，v处的boundingbox，边界是当前位置的临近4个像素
+        auto u_min = std::floor(u_img);
+        auto u_max = std::min((float)width, std::ceil(u_img));
+        auto v_min = std::floor(v_img);
+        auto v_max = std::min((float)height, std::ceil(v_img));
+
+        // 四个临近像素的颜色值，然后进行插值操作
+        // opencv中的v方向和贴图中的v方向是反的，v_min实际上在v_max上面
+        auto u_00 = image_data.at<cv::Vec3b>(v_max, u_min);
+        auto u_10 = image_data.at<cv::Vec3b>(v_max, u_max);
+        auto u_01 = image_data.at<cv::Vec3b>(v_min, u_min);
+        auto u_11 = image_data.at<cv::Vec3b>(v_min, u_max);
+
+        // 待插值像素在bbx中的位置
+        float s = (u_img - u_min) / (u_max - u_min);
+        float t = (v_img - v_max) / (v_min - v_max);
+
+        auto u_0 = (1 - s) * u_00 + s * u_10;
+        auto u_1 = (1 - s) * u_01 + s * u_11;
+
+        auto F = (1 - t) * u_0 + t * u_1;
+
+        return Eigen::Vector3f(F[0], F[1], F[2]);
     }
 
 };
